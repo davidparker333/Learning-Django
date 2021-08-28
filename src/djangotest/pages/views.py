a190e2a2a5c9eb05407b3abb665c0d7743242598
David Parker <davidparker333@gmail.com>
2021-08-28 17:29:07 -0400

    boilerplate

---
+++ src/djangotest/pages/views.py
@@ -0,0 +1,8 @@
+from django.http.response import JsonResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home_view(*args, **kwargs):
+    return HttpResponse("Hello world!")
