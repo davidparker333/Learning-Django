88ba63cbcd08f147712138101cfd4fb105e27c5e
David Parker <davidparker333@gmail.com>
2021-09-04 19:11:42 -0400

    saving posts on form submission

--- src/djangotest/pages/views.py
+++ src/djangotest/pages/views.py
@@ -2,6 +2,7 @@ from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.forms import NewPostForm
+from blog.models import Post
 
 # Create your views here.
 
@@ -16,6 +17,10 @@ def add_post_view(request, *args, **kwargs):
     if request.method == 'POST':
         form = NewPostForm(request.POST)
         if form.is_valid():
+            title = form.cleaned_data['title']
+            body = form.cleaned_data['body']
+            p = Post(title=title, body=body)
+            p.save()
             return render(request, 'home.html', context)
     else:
         form = NewPostForm()
