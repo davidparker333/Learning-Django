346cc358a6a36e9731beeeba19978b928effdd74
David Parker <davidparker333@gmail.com>
2021-09-11 13:28:32 -0400

    using model form, styling form

--- src/djangotest/blog/forms.py
+++ src/djangotest/blog/forms.py
@@ -1,6 +1,18 @@
 from django import forms
+from .models import Post
 
-class NewPostForm(forms.Form):
-    
-    title = forms.CharField(max_length=120, required=True)
-    body = forms.CharField(max_length=500, required=True)
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'body'
+        ]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control my-1'}),
+            'body': forms.Textarea(attrs={'class': 'form-control my-1'})
+        }
+
+        # If I was using a regular form, not a model form
+        # title = forms.CharField(max_length=120, required=True),
+        # body = forms.CharField(max_length=500, required=True)
--- src/djangotest/pages/views.py
+++ src/djangotest/pages/views.py
@@ -4,8 +4,6 @@ from django.http import HttpResponse
 from blog.forms import NewPostForm
 from blog.models import Post
 
-# Create your views here.
-
 def home_view(request, *args, **kwargs):
     # return HttpResponse("Hello world!")
     context = {}
@@ -17,10 +15,12 @@ def add_post_view(request, *args, **kwargs):
     if request.method == 'POST':
         form = NewPostForm(request.POST)
         if form.is_valid():
-            title = form.cleaned_data['title']
-            body = form.cleaned_data['body']
-            p = Post(title=title, body=body)
-            p.save()
+            # If I was using a regular form, not model form
+            # title = form.cleaned_data['title']
+            # body = form.cleaned_data['body']
+            # p = Post(title=title, body=body)
+            # p.save()
+            form.save()
             return render(request, 'home.html', context)
     else:
         form = NewPostForm()
